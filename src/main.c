6c8baa5fc529571f1b5383f2b16fc9ed00abc8d0
smplusr <83000221+smplusr@users.noreply.github.com>
2022-05-23 12:56:05 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main (int argc, char *argv[]) {
 	LoadLib ();
 	char s[4096];
 
-	if (!argv[1]) { Input (s); Remove (s, "	 "); Parse (s, 0); Run (lambda[0]); DEBUG_PrintLambda (lambda); }
+	if (!argv[1]) { Input (s); Remove (s, "	 "); Parse (s, 0); Eval (lambda[0]); DEBUG_PrintLambda (lambda); }
 	for (int i = 0; argv[i]; i++) {
 		if (argv[i][0] == '-' && argv[i][1]) {
 			switch (argv[i][1]) {
@@ -15,13 +15,13 @@ int main (int argc, char *argv[]) {
 					Input (s); 
 					Remove (s, "	 "); 
 					Parse (s, 0);
-					Run (lambda[0]);
+					Eval (lambda[0]);
 					break;
 				case 'f':
 					Read (s, argv[i+1]);
 					Remove (s, "\n	"); 
 					Parse (s, 0);
-					Run (lambda[0]);
+					Eval (lambda[0]);
 					break;
 				case 'd':
 					DEBUG_PrintLambda (lambda);
