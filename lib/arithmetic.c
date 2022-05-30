09344c156857790b4a69cb5ad40bf6063573302d
smplusr <83000221+smplusr@users.noreply.github.com>
2022-05-30 12:12:52 +0200

    Delete lib directory

--- lib/arithmetic.c
+++
@@ -1,8 +0,0 @@
-#include "lib.h"
-
-
-void *add (void (***a)) { 
-	int b = 0;
-	for (int i = 0; a[1][i]; i++) b += (int)cons (a[1][i]);
-	return b;
-}
--- lib/interface.c
+++
@@ -1,9 +0,0 @@
-#include "lib.h"
-
-
-void command (void (****a)) { 
-	char b[256] = "";
-	for (int i = 0; a[1][i]; i++)
-		b[i] = (int)cons (a[1][i]);
-	system (b);
-}
